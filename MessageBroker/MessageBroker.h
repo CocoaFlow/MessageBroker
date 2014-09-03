be4e8f4dea341a640203f67079898dce5b2077fd
Paul Young <paulyoungonline@gmail.com>
2014-09-02 22:11:27 -0400

    Initial Commit

---
+++ MessageBroker/MessageBroker.h
@@ -0,0 +1,19 @@
+//
+//  MessageBroker.h
+//  MessageBroker
+//
+//  Created by Paul Young on 02/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MessageBroker.
+FOUNDATION_EXPORT double MessageBrokerVersionNumber;
+
+//! Project version string for MessageBroker.
+FOUNDATION_EXPORT const unsigned char MessageBrokerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageBroker/PublicHeader.h>
+
+
